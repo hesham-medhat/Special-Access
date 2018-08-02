82ba595a49c4d813b8847bbe7c4a43ac9b5cda6b
Hesham Medhat <heshammedhat5@gmail.com>
2018-08-02 15:39:08 +0200

    Implement ValidateBySqlConnection using the stored procedure validateUserByUP

--- ADLogin/Models/Validation/UPValidator.cs
+++ ADLogin/Models/Validation/UPValidator.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using System.Data.Common;
 
 namespace ADLogin.Models.Validation
 {
@@ -16,12 +18,10 @@ namespace ADLogin.Models.Validation
             loginCredentials = candidate;
         }
 
-        public bool Validate() {
+        private bool ValidateByEntity(string inputUsername, string inputPassword)
+        {
             using (LoginDBEntities db = new LoginDBEntities())
             {
-                string inputUsername = loginCredentials.username;
-                string inputPassword = loginCredentials.password;
-
                 user foundUser = db.users.SingleOrDefault(user => user.username.Equals(inputUsername, StringComparison.Ordinal));
                 if (foundUser == null || foundUser.password != inputPassword)
                 {
@@ -32,6 +32,51 @@ namespace ADLogin.Models.Validation
                     return true;
                 }
             }
+        }
+
+        private bool ValidateBySqlConnection(ref string inputUsername, ref string inputPassword)
+        {
+            string connectionString;
+            using (LoginDBEntities db = new LoginDBEntities())
+            {
+                connectionString = db.Database.Connection.ConnectionString;
+            }
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand()
+                {
+                    CommandText = "validateUserByUP",
+                    Connection = connection,
+                    CommandType = System.Data.CommandType.StoredProcedure
+                })
+                {
+                    command.Parameters.Add(new SqlParameter("@username", inputUsername));
+                    command.Parameters.Add(new SqlParameter("@password", inputPassword));
+
+                    // Adding parameter for the returned value.
+                    SqlParameter returnValueParameter = new SqlParameter("returner", System.Data.SqlDbType.Int);
+                    returnValueParameter.Direction = System.Data.ParameterDirection.ReturnValue;
+                    command.Parameters.Add(returnValueParameter);
+
+                    command.ExecuteNonQuery();
+                    int returner = (int) command.Parameters["returner"].Value;
+                    return returner == 1;
+                }
+
+            }
+        }
+
+        public bool Validate() {
+
+            
+            string inputUsername = loginCredentials.username;
+            string inputPassword = loginCredentials.password;
+
+            return ValidateBySqlConnection(ref inputUsername, ref inputPassword);
+
+            //Another method: return ValidateByEntity(inputUsername, inputPassword);
+            
         }
     }
 }
